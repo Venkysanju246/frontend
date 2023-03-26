6b4e23cac62f7d3361bfae9dee08964b86ae4015
Venkysanju246 <sanjuvenky246@gmail.com>
2023-03-26 14:39:35 +0530

    fe

---
+++ src/findex.js
@@ -0,0 +1,53 @@
+let form = document.querySelector("form")
+let h1 = document.getElementById("regdisplay")
+
+form.addEventListener("submit", (e)=>{
+    let username = form.username.value
+let email = form.email.value
+let password = form.password.value
+let locationel = form.location.value
+    console.log("click")
+    e.preventDefault()
+    let regobj = {
+        username:username,
+        email:email,
+        password:password,
+        location:locationel
+    }
+    console.log(regobj)
+    
+
+    fetch("https://thankful-puce-fedora.cyclic.app/user/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(regobj),
+    }).then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+         h1.innerText = "Registration Successfull Buddy!!"
+    alert("Registration is done!")
+   window.location.href = "login.html"
+
+    // fetch("http://localhost:8080/user/register",{
+    //     method:"POST",
+    //     headers:{
+    //         "Content-Type":"application/json"
+    //     },
+    //     body:JSON.stringify(regobj)
+
+    // }).then((response)=>{
+        
+    //     response.json()
+    // }).then((data)=>{
+    //     console.log(data)
+    // }).catch((err)=>{
+    //     console.log(err)
+    // })
+   
+})
